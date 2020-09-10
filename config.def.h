93ff31e0e214fd95afd81a6e55cbb4387265015e
William Ferreira <wqferr@gmail.com>
2020-09-10 10:15:52 -0300

    Change a few settings

--- config.def.h
+++ config.def.h
@@ -33,8 +33,8 @@ static const unsigned int alphas[][3] = {
 };
 
 /* -l and -g options; controls number of lines and columns in grid if > 0 */
-static unsigned int lines = 0;
-static unsigned int columns = 0;
+static unsigned int lines = 4;
+static unsigned int columns = 3;
 
 /*
  * Characters not considered part of a word while deleting words
